7b6ef5f315069b8da0356bfa9d378cbd7e881098
Detroitasd <pedrosso395@gmail.com>
2025-10-23 22:15:34 +0900

    messenger.py

--- messenger.py
+++ messenger.py
@@ -788,4 +788,4 @@ def handle_webrtc_ice_candidate(data):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
